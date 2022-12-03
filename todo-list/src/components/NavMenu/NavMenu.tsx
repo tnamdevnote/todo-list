aa0321a3035e63080a864721eae4297cf4c40a58
Taek Been Nam <taekbeen93@gmail.com>
2022-12-03 16:37:23 -0500

    style: adjust layout

--- todo-list/src/components/NavMenu/NavMenu.tsx
+++ todo-list/src/components/NavMenu/NavMenu.tsx
@@ -9,13 +9,13 @@ interface NavMenuProps {
 export default function NavMenu({ filters, activeFilter, onChange }: NavMenuProps) {
   return (
     <header className={styles.header}>
-      <button type="button" className={styles.toggle}>
+      <button className={styles.toggle} type="button">
         <WbSunnyIcon />
       </button>
       <ul className={styles.menu}>
         {filters.map((filter) => {
           return (
-            <li key={filter}>
+            <li className={styles.menuItem} key={filter}>
               <button
                 className={`${styles.menuButton} ${filter === activeFilter ? styles.active : ''}`}
                 type="button"
